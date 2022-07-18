aea0e9f8738fa67c255af7386faee95c7bf640a5
ian_rakhmatullin <ravenglc1@gmail.com>
2022-07-18 23:51:42 +0500

    Update to hadoop 3.3.3 and spark 3.3.0

--- sparkApp/src/main/scala/r/ian/spark/job/lowLevelRDD/KeyValueRDDJob.scala
+++ sparkApp/src/main/scala/r/ian/spark/job/lowLevelRDD/KeyValueRDDJob.scala
@@ -39,34 +39,39 @@ class KeyValueRDDJob extends Job{
         .persist(StorageLevel.MEMORY_ONLY_SER)
 
 //    keyWordRdd
-//      .lookup('S')
-//      .foreach(charic => println(charic))
-//
-//
-//    val chars = words.flatMap(word => word.toLowerCase.toSeq)
-//    val KVcharacters = chars.map(letter => (letter, 1))
-//
-//    //NOTE OOM prone since all values for a given key should
-//    //present on the same executor
-//    //its better to use
-//    //KVcharacters.reduceByKey(addFunc).collect()
-//
-////    KVcharacters
-////      .groupByKey()
-////      .map(row => (row._1, row._2.reduce(addFunc)))
-////      .collect()
-//
-//
-//    //zipping. Both RDDs should have the same number of partitions and elements!
-//    val numRange = spark.sparkContext.parallelize(0 to 9, 2)
-//    val zipped = words.zip(numRange)
-//    zipped
 //      .collect()
-//      .foreach(a => println(a))
-//
-//  }
-//
-//
-//  def maxFunc(left:Int, right:Int) = math.max(left, right)
-//  def addFunc(left:Int, right:Int) = left + right
-//}
+//      .foreach(x => println(x))
+
+
+    keyWordRdd
+      .lookup('S')
+      .foreach(charic => println(charic))
+
+
+    val chars = words.flatMap(word => word.toLowerCase.toSeq)
+    val KVcharacters = chars.map(letter => (letter, 1))
+
+    //NOTE OOM prone since all values for a given key should
+    //present on the same executor
+    //its better to use
+    //KVcharacters.reduceByKey(addFunc).collect()
+
+//    KVcharacters
+//      .groupByKey()
+//      .map(row => (row._1, row._2.reduce(addFunc)))
+//      .collect()
+
+
+    //zipping. Both RDDs should have the same number of partitions and elements!
+    val numRange = spark.sparkContext.parallelize(0 to 9, 2)
+    val zipped = words.zip(numRange)
+    zipped
+      .collect()
+      .foreach(a => println(a))
+
+  }
+
+
+  def maxFunc(left:Int, right:Int) = math.max(left, right)
+  def addFunc(left:Int, right:Int) = left + right
+}
